e215bcf1f62788441c27f7af51af6801cbcc0d3f
uniquesa <muhammadnazar62927@gmail.com>
2025-08-30 01:47:15 +0500

    nazar-committe 4th

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    use AuthenticatesUsers;  
     protected function authenticated(Request $request, $user)
     {
         if ($user->roles->isEmpty()) {
@@ -27,4 +29,5 @@ class LoginController extends Controller
 
         return redirect('/');
     }
+    
 }
