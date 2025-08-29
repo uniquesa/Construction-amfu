1722e023eb38e2ebb27b6d616556a3e4de282c12
ZuhaibSh <zzuhaib176@gmail.com>
2025-08-30 01:36:20 +0500

    zuhaib 4th commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasRoles;
+    use HasFactory, Notifiable, HasRoles;
     // ... baaki code
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\UserManagementController;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/index',[ViewController::class,'index']); 
 // Route::get('/index',[ViewController::class,'index']);
@@ -33,4 +38,5 @@ Route::middleware(['role:Admin'])->group(function () {
     Route::get('/Admin/user', [UserManagementController::class, 'index'])->name('Admin.user');
     Route::post('/admin/users/{id}/assign-role', [UserManagementController::class, 'assignRole'])->name('admin.users.assignRole');
 });
-
+Route::get('/admin/dashboard', [App\Http\Controllers\Auth\LoginController::class, 'dashboard'])
+    ->name('Admin.admin-dashboard');
