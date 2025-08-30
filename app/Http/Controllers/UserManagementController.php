6161be372ee11b3b7e6490dafb282ce99167822d
uniquesa <muhammadnazar62927@gmail.com>
2025-08-30 19:16:54 +0500

    commit by nazar 6th

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Spatie\Permission\Models\Role;
 use App\Models\User;
 
 class UserManagementController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        return view('Admin.user-management', compact('users'));
+       $users = User::with('roles')->get();
+        $roles = Role::all(); // sare roles bhejne
+        return view('admin.user-management', compact('users', 'roles'));
     }
 
-    public function assignRole(Request $request, $id)
+    public function assignRole(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
+        $request->validate([
+            'role' => 'required|exists:roles,name',
+        ]);
+
+        // purana role hata kar naya assign
         $user->syncRoles([$request->role]);
 
         return redirect()->back()->with('success', 'Role assigned successfully!');
     }
+    
 }
