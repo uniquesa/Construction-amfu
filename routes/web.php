675df00d81660878afbbde80b5e3a17e13252106
ZuhaibSh <zzuhaib176@gmail.com>
2025-08-31 11:52:12 +0500

    5th commit zuhaib

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/index',[ViewController::class,'index']);
 // Route::get('/index',[ViewController::class,'index']);
 Route::get('/login',[ViewController::class,'login']);
 Route::get('/ZUHAIB',[ViewController::class,'ZUHAIB']);
-Route::get('/ZUHAIB2',[ViewController::class,'ZUHAIB2']);
 Route::get('/signup',[ViewController::class,'signup']);
 Route::get('/users',[ViewController::class,'users']);
 Route::get('/form',[ViewController::class,'form']);
